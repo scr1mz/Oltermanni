686302eb135f84ea73ba5917105de4f50f480f98
isakov <isakov.kostik@outlook.com>
2023-09-05 19:51:24 +0300

    slider changed on swiper

--- src/components/Promo/Promo.tsx
+++ src/components/Promo/Promo.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import "./Promo.scss"
 import "/src/fonts.scss"
 import {ProductSlider} from "../PromoSlider/PromoSlider";
+import {ProductSwiper} from "../PromoSwiper";
 
 export const Promo = () => {
     return(
         <section className={"body-content__promo-section"}>
             <h1>в акции участвуют</h1>
             <div className={"promo-section__slider-container"}>
-                <ProductSlider/>
+                <ProductSwiper/>
             </div>
         </section>
     );
