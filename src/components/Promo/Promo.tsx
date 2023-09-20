496816c855ac9d21597ec2267bbeac0b65a2b92a
isakov <isakov.kostik@outlook.com>
2023-09-21 00:10:59 +0300

    style and &:hover{

--- src/components/Promo/Promo.tsx
+++ src/components/Promo/Promo.tsx
@@ -7,7 +7,7 @@ import { ProductSwiper } from '../PromoSwiper/PromoSwiper'
 export const Promo: React.FC = () => {
   return (
         <section className={'body-content__promo-section'}>
-            <h1>в акции участвуют</h1>
+            <h1 className={'promo-title'}>в акции участвуют</h1>
             <div className={'promo-section__slider-container'}>
                 <ProductSwiper/>
             </div>
